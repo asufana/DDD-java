044c1e1d1e3adbd270c220270f65191901046323
Makoto Hanafusa <asufanah@gmail.com>
2015-04-02 14:16:48 +0000

    Maven artifacts for 1.0

--- src/main/java/com/github/asufana/ddd/vo/annotations/Pattern.java
+++
@@ -1,9 +0,0 @@
-package com.github.asufana.ddd.vo.annotations;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface Pattern {
-    String regex();
-}
